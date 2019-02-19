4cb4511cecabf35f2ddea9d9c69250def095568e
carlos <cadgo@hotmail.com>
2019-02-19 10:30:27 -0600

    asdsad

--- multiformtest/forms.py
+++ multiformtest/forms.py
@@ -3,5 +3,6 @@ from django.forms import formset_factory
 
 class TitleBook(forms.Form):
     BookName = forms.CharField(label='Nombre del Libro', max_length=20)
+    Aditional = forms.CharField(label='Aditional', max_length=20)
 
-BookNameFormSet = formset_factory(TitleBook, extra=2, max_num=10)
+BookNameFormSet = formset_factory(TitleBook, min_num=1,extra=0, max_num=10)
